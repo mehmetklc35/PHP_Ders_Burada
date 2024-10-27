491c8312bfb4c5133fdd16615820223bd14c3461
Your Name <you@example.com>
2024-10-27 20:44:51 +0300

    projeye devam edildi....

--- admin/contents.php
+++ admin/contents.php
@@ -6,32 +6,8 @@
       }else{
             $tutor_id = '';
             header('location: login.php');
-      }
-
-      if (isset($_GET['get_id'])) {
-            $get_id = $_GET['get_id'];
-      }else{
-            $get_id = '';
-            header('location:playlist.php');      
-      }      
-     
-      //delete playlist
-
-      if (isset($_POST['delete'])) {
-            $delete_id = $_POST['playlist_id'];
-
-            $delete_playlist_thumb = $conn->prepare("SELECT * FROM `playlist` WHERE id = ? LIMIT 1");
-                  $delete_playlist_thumb->execute([$delete_id]);
-                  $fetch_thumb = $delete_playlist_thumb->fetch(PDO::FETCH_ASSOC);
-                  unlink('../uploaded_files/'.$fetch_thumb['thumb']);
-
-                  $delete_bookmark = $conn->prepare("DELETE FROM `bookmark` WHERE playlist_id = ?");
-                  $delete_bookmark->execute([$delete_id]);
-                  $delete_playlist = $conn->prepare("DELETE FROM `playlist` WHERE id = ?");
-                  $delete_playlist->execute([$delete_id]);
-                  header('location:playlists.php');
-      }
-
+      }    
+      
       if (isset($_POST['delete_video'])) {
             $delete_id = $_POST['video_id'];
         
@@ -61,9 +37,8 @@
             } else {
                 $message[] = 'Video bulunamadı veya zaten silinmiş.';
             }
-      }  
-
-      
+      }
+        
 ?>
 <style type="text/css">
       <?php include '../css/admin_style.css'; ?>
@@ -73,60 +48,23 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>add playlists</title>
+      <title>added playlists</title>
       <!-- boxicon link -->
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>      
 
 </head>
 <body>
       <?php include '../components/admin_header.php'; ?>
-      <section class="view-playlist">
-            <h1 class="heading">playlist detail</h1>
-
-            <?php
-                  $select_playlist = $conn->prepare("SELECT * FROM `playlist` WHERE id = ? AND tutor_id = ?");
-                  $select_playlist->execute([$get_id, $tutor_id]);
-                  if ( $select_playlist->rowCount() > 0 ) {
-                        while($fetch_playlist = $select_playlist->fetch(PDO::FETCH_ASSOC)) {
-                              $playlist_id = $fetch_playlist['id'];
-                              $count_videos = $conn->prepare("SELECT * FROM `content` WHERE playlist_id = ?");
-                              $count_videos->execute([$playlist_id]);
-                              $total_videos = $count_videos->rowCount();                       
-            
-            ?>
-            <div class="row">
-                  <div class="thumb">
-                        <span><?= $total_videos; ?> </span>
-                        <img src="../uploaded_files/<?= $fetch_playlist['thumb']; ?>" alt="">
-                  </div>
-                  <div class="details">
-                        <h3 class="title"><?= $fetch_playlist['title']; ?> </h3>
-                        <div class="date"><i class="bx bxs-calendar-alt"></i><span><?= $fetch_playlist['date']; ?> </span></div>
-                  </div>
-                  <div class="description">
-                        <?= $fetch_playlist['description']; ?> 
-                  </div>
-                  <form action="" method="post" class="flex-btn">
-                        <input type="hidden" name="playlist_id" value="<?= $playlist_id; ?> ">
-                        <a href="update_playlist.php?get_id=<?= $playlist_id; ?>" class="btn">update playlist</a>
-                        <input type="submit" name="delete" value="delete playlist" class="btn" onclick="return confirm('delete this playlist');">
-                  </form>
-            </div>
-            <?php
-                        }
-                  }else {
-                        echo '<p class="empty"> no playlist added yet! </p>';
-                  }
-            ?>
-           
-      </section>
       <section class="contents">
-            <h1 class="heading">playlist videos</h1>
+            <h1 class="heading">your contents</h1>
 
-            <div class="box-container">                  
+            <div class="box-container">
+                  <div class="add">
+                        <a href="add_content.php"> <i class="bx bx-plus"></i> </a>
+                  </div> 
                   <?php
-                        $select_videos = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ? AND playlist_id = ?");
-                        $select_videos->execute([$tutor_id, $playlist_id]);
+                        $select_videos = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ? ORDER BY date DESC");
+                        $select_videos->execute([$tutor_id]);
 
                         if ($select_videos->rowCount() > 0) {
                               while($fetch_videos = $select_videos->fetch(PDO::FETCH_ASSOC)) {
@@ -152,15 +90,10 @@
                   <?php 
                               }
                         }else {
-                              echo '
-                                    <div class="empty">
-                                          <p style="margin-bottom: 1.5rem;">no video added yet!</p>
-                                          <a href="add_content.php" class="btn" style="margin-top: 1.5rem;">add videos</a>
-                                    </div> 
-                              ';
-                        }                
-                  ?>
-                                      
+                              echo '<p class="empty">no video adden in playlist yet!</p>';
+                        }                 
+                  
+                  ?>                      
             </div>
             
 
